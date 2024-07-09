6664eedbe99c7a8612974420f80e2fb8a360b515
SleepyTK <75376380+SleepyTK@users.noreply.github.com>
2024-07-09 12:52:07 +0200

    3d carousel structure

--- components/head/loadHead.js
+++ components/head/loadHead.js
@@ -2,4 +2,5 @@ fetch('components/head/head.html')
     .then(response => response.text())
     .then(data => {
         document.head.innerHTML += data;
-    });
+    })
+    .catch(error => console.error('Error loading head:', error));
